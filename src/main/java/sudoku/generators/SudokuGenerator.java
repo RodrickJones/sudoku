83c3ac609210b1326dd10ccf30e25fd7becce555
Rodrick Jones <rodrickgjones@gmail.com>
2018-01-07 16:56:00 +1100

    SudokuGenerator  - Replaced recursion with a loop
    Signed-off-by: Rodrick Jones <rodrickgjones@gmail.com>

--- src/main/java/sudoku/generators/SudokuGenerator.java
+++ src/main/java/sudoku/generators/SudokuGenerator.java
@@ -17,35 +17,34 @@ public class SudokuGenerator {
 
     public static int[][] generate(int n) {
         Alert genAlert = new Alert(Alert.AlertType.INFORMATION, "Generating Sudoku Puzzle");
-        try {
-            genAlert.show();
-            return generate(n, 0);
-        } finally {
-            genAlert.close();
-        }
-    }
-
-    private static int[][] generate(int n, int depth) {
-        int[][] result = new int[n][n];
-        System.out.println("Depth=" + depth++);
+        genAlert.show();
+        int[][] result;
         int rng = (int) ((Math.random() * 2 + 1) / 4 * n * n);
         System.out.println(rng + "/" + n * n);
         List<Integer> domain = IntStream.rangeClosed(1, n).boxed().collect(Collectors.toList());
-        for (int i = 0; i < rng; i++) {
-            int c = (int) (Math.random() * n);
-            int r = (int) (Math.random() * n);
-            Collections.shuffle(domain);
-            for (int v : domain) {
-                if (Solver.canSelect(result, c, r, v)) {
-                    result[c][r] = v;
-                    break;
+        Solver solver;
+        int iteration = 0;
+        do {
+            System.out.println("Generating board, iteration " + iteration);
+            result = new int[n][n];
+            for (int i = 0; i < rng; i++) {
+                int c = (int) (Math.random() * n);
+                int r = (int) (Math.random() * n);
+                Collections.shuffle(domain);
+                for (int v : domain) {
+                    if (Solver.canSelect(result, c, r, v)) {
+                        result[c][r] = v;
+                        break;
+                    }
                 }
             }
-        }
-        Solver solver = new DepthFirstSearchSolver(result);
-        System.out.println("Checking solution");
-        solver.findSingleSolution();
-        System.out.println(solver.isSolvable());
-        return solver.isSolvable() ? result : generate(n, depth);
+            System.out.println("Checking board for solution");
+            solver = new DepthFirstSearchSolver(result);
+            solver.findSingleSolution();
+            iteration++;
+        } while (!solver.isSolvable());
+        System.out.println("Board is solvable!");
+        genAlert.close();
+        return result;
     }
 }
